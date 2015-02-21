fd3a0ee24f66ab41e9947079d49386a75890d829
Wim Munters <wim.munters@kuleuven.be>
2015-02-21 10:50:21 +0100

    Added some features

--- loading/load_sp.py
+++ loading/load_sp.py
@@ -1,5 +1,19 @@
 import numpy as np
 
+class Turbine:
+    def __init__(self, turb):
+        self.x = turb[0]
+        self.y = turb[1]
+        self.H = turb[2]
+        self.r = turb[4]
+
+def load_windfarm(filename):
+    dummy = np.loadtxt(filename, skiprows=2)
+    windfarm = [] # Empty list
+    for turbine_data in dummy:
+        windfarm.append(Turbine(turbine_data))
+    return windfarm
+
 def load_NSp(filename):
     dummy = np.loadtxt(filename, skiprows=1)
     NSp = {}
@@ -57,9 +71,9 @@ def load_field(filename, N1, N2, N3, N4):
     return u
 
 def load_windpower(filename):
-    dummy = np.loadtxt(filename)
+    dummy = np.loadtxt(filename,comments='%')
     time = dummy[:,0]
-    power = dummy[:,2::2]
+    power = -dummy[:,2::2]
     return time, power
 
 def load_BLfieldstat(filename, N1, N2, N3):
@@ -84,14 +98,81 @@ def load_BLfieldstat(filename, N1, N2, N3):
     stat['wst'] = u[:,:,:,10]
     return stat
 
-def load_stream_spec(filename, Lx, Nx2, Nz):
+def load_stream_spec(filename, L, N, Nz):
     dummy = np.loadtxt(filename, comments='%')
     spec = {}
-    spec['k']  = [(i)/Lx*(2*np.pi) for i in range(Nx2/2)]
+    spec['k']  = [(i)/L*(2*np.pi) for i in range(N/2)]
     spec['z']  = dummy[0:Nz-1,0]
     spec['uu'] = dummy[0:Nz-1,1:]
     spec['vv'] = dummy[Nz:2*Nz-1,1:]
+    spec['zst']= dummy[2*Nz-1:,0]
     spec['ww'] = dummy[2*Nz-1:,1:]
-    spec['zst'] = dummy[2*Nz-1:,0]
     return spec
 
+def load_BLfieldstat_bin(filename, N1, N2, N3):
+    N4 = 11
+    stat = {}
+    with open(filename, 'rb') as binfile:
+        stat['nsamp'] = np.fromfile(binfile, dtype=np.int32, count=1)
+        stat['time_interv'] = np.fromfile(binfile, dtype=np.float32, count=1)
+        stat['time_incurr'] = np.fromfile(binfile, dtype=np.float32, count=1)
+        dumm = np.fromfile(binfile, dtype=np.float64)
+        shape = (N1,N2,N3,N4)
+        dumm = dumm.reshape(shape, order='F')
+        stat['u']   = dumm[:,:,:,0]
+        stat['v']   = dumm[:,:,:,1]
+        stat['w']   = dumm[:,:,:,2]
+        stat['uu']  = dumm[:,:,:,3]
+        stat['vv']  = dumm[:,:,:,4]
+        stat['ww']  = dumm[:,:,:,5]
+        stat['uv']  = dumm[:,:,:,6]
+        stat['uw']  = dumm[:,:,:,7]
+        stat['vw']  = dumm[:,:,:,8]
+        stat['p']   = dumm[:,:,:,9]
+        stat['wst'] = dumm[:,:,:,10]
+    return stat
+
+def load_BLfield(filename, N1, N2, N3):
+    N1 = N1/2+1
+    BL = {}
+    with open(filename, 'rb') as binfile:
+        BL['time'] = np.fromfile(binfile, dtype=np.float64, count=1)
+        BL['Lx'] = np.fromfile(binfile, dtype=np.float64, count=1)
+        BL['Ly'] = np.fromfile(binfile, dtype=np.float64, count=1)
+        BL['Nx2'] = np.fromfile(binfile, dtype=np.int32, count=1)
+        BL['Ny'] = np.fromfile(binfile, dtype=np.int32, count=1)
+        BL['Nz'] = np.fromfile(binfile, dtype=np.int32, count=1)
+        BL['thetaground'] = np.fromfile(binfile, dtype=np.float64, count=1)
+        dum = np.fromfile(binfile,dtype=np.complex128)
+
+    amount = N1*N2*N3
+    shape  = (N1, N2, N3)
+    shape2 = (N1, N2, N3-1)
+    BL['uu'] = dum[:amount].reshape(shape, order='F')
+    BL['vv'] = dum[amount:2*amount].reshape(shape, order='F')
+    BL['ww'] = dum[2*amount:].reshape(shape2, order='F')
+    
+    post = True
+    BLpostkeys = ['uu','vv','ww']
+    if post:
+        for key in BLpostkeys:
+        # Take the logs
+            BL[key] = np.log(np.abs(BL[key]))   
+        # Shift spectrum to match correct locations
+            BL[key] = np.fft.fftshift(BL[key],axes=(1,))
+        # Remove the defunct wavenumbers
+            BL[key] = BL[key][:N1-1, 1:] 
+
+    return BL
+
+def head_BLfield(filename):
+    head = {}
+    with open(filename, 'rb') as binfile:
+        head['time'] = np.fromfile(binfile, dtype=np.float64, count=1)
+        head['Lx'] = np.fromfile(binfile, dtype=np.float64, count=1)
+        head['Ly'] = np.fromfile(binfile, dtype=np.float64, count=1)
+        head['Nx2'] = np.fromfile(binfile, dtype=np.int32, count=1)
+        head['Ny'] = np.fromfile(binfile, dtype=np.int32, count=1)
+        head['Nz'] = np.fromfile(binfile, dtype=np.int32, count=1)
+        head['thetaground'] = np.fromfile(binfile, dtype=np.float64, count=1)
+    return head

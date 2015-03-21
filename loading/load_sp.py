f5b2f55d9a647a4dda6b7298633ed217a47a95ca
Wim Munters <wim.munters@gmail.com>
2015-03-21 15:42:57 +0100

    Moved cube slider from loading to plotting

--- loading/load_sp.py
+++ loading/load_sp.py
@@ -172,39 +172,6 @@ def load_BLfield_real(filename, N1, N2, N3):
 
     return BL
 
-def cube_show_slider(cube, axis=2, **kwargs):
-    import matplotlib.pyplot as plt
-    from matplotlib.widgets import Slider, Button, RadioButtons
-    # check dim
-    if not cube.ndim == 3:
-        raise ValueError("cube should be an ndarray with ndim == 3")
-    # generate figure
-    fig = plt.figure()
-    ax = plt.subplot(111)
-    fig.subplots_adjust(left=0.25, bottom=0.25)
-
-    # select first image
-    s = [slice(0, 1) if i == axis else slice(None) for i in xrange(3)]
-    im = cube[s].squeeze()
-
-    # display image
-    l = ax.imshow(im, **kwargs)
-    axcolor = 'lightgoldenrodyellow'
-    ax = fig.add_axes([0.25, 0.1, 0.65, 0.03], axisbg=axcolor)
-        
-    slider = Slider(ax, 'Axis %i index' % axis, 0, cube.shape[axis] - 1,
-                                        valinit=0, valfmt='%i')
-            
-    def update(val):
-        ind = int(slider.val)
-        s = [slice(ind, ind + 1) if i == axis else slice(None) for i in xrange(3)]
-        im = cube[s].squeeze()
-        l.set_data(im, **kwargs)
-        fig.canvas.draw()
-        
-    slider.on_changed(update)
-                                                                
-    plt.show()
 
 def load_BLfield(filename, N1, N2, N3):
     N1 = N1/2+1

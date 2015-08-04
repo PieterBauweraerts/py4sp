facdb6032db6c9fc3983560943330f0c3d208c6d
Wim Munters <wim.munters@kuleuven.be>
2015-08-04 13:27:09 +0200

    Added writing module

---
+++ writing/write_sp.py
@@ -0,0 +1,38 @@
+import numpy as np
+import fft_sp as fft
+
+def write_BLfield(field, filename, spectral=False):
+    print '##########################################'
+    print '# Reading field to file ', filename
+    print '# Input field is spectral? ', spectral
+    print '# ------------------------------------------'
+    print '# Field information'
+    print '# ------------------------------------------'
+    print '# time  = ', field['time']
+    print '# Lx    = ', field['Lx']
+    print '# Ly    = ', field['Ly']
+    print '# Nx2   = ', field['Nx2']
+    print '# Ny    = ', field['Ny']
+    print '# Nz    = ', field['Nz']
+    print '# theta = ', field['thetaground']
+    print '##########################################'
+
+    with open(filename,'wb') as binfile:
+        field['time'].tofile(binfile)
+        field['Lx'].tofile(binfile)
+        field['Ly'].tofile(binfile)
+        field['Nx2'].tofile(binfile)
+        field['Ny'].tofile(binfile)
+        field['Nz'].tofile(binfile)
+        field['thetaground'].tofile(binfile)
+
+        if spectral:
+            field['uu'].tofile(binfile)
+            field['vv'].tofile(binfile)
+            field['ww'].tofile(binfile)
+
+        else:
+            fft.r2c(field['uu'], field['Nx2'], field['Ny']).tofile(binfile)
+            fft.r2c(field['vv'], field['Nx2'], field['Ny']).tofile(binfile)
+            fft.r2c(field['ww'], field['Nx2'], field['Ny']).tofile(binfile)
+

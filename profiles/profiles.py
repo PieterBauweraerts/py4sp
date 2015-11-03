58c18c2a3f14296c76a0a84ba0d49ecd0581555a
Wim Munters <wim.munters@kuleuven.be>
2015-11-03 15:30:49 +0100

    Bugfix in profiles

--- profiles/profiles.py
+++ profiles/profiles.py
@@ -56,11 +56,11 @@ class Profile:
             r = ref
         elif ('zhub' in kwargs):
             k1 = np.max(np.where(self.z <= kwargs['zhub']))
-	    u1 = self.U[k1]
-	    u2 = self.U[k1+1]
-	    z1 = self.z[k1]
-	    z2 = self.z[k1+1]
-	    # Linear interpolation to exact hub height
+            u1 = self.U[k1]
+            u2 = self.U[k1+1]
+            z1 = self.z[k1]
+            z2 = self.z[k1+1]
+            # Linear interpolation to exact hub height
             r = u1 + (kwargs['zhub'] - z1)/(z2 - z1)*(u2 - u1)
         else:
             r = 1.

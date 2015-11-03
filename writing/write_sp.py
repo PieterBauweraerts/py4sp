067350759b240339516ebf09b12b96790413e17e
Wim Munters <wim.munters@kuleuven.be>
2015-11-03 15:42:56 +0100

    Added documentation to writing module

--- writing/write_sp.py
+++ writing/write_sp.py
@@ -2,20 +2,32 @@ import numpy as np
 import fft_sp as fft
 
 def write_BLfield(field, filename, spectral=False):
-    print '##########################################'
-    print '# Writing field to file ', filename
-    print '# Input field is spectral? ', spectral
-    print '# ------------------------------------------'
-    print '# Field information'
-    print '# ------------------------------------------'
-    print '# time  = ', field['time']
-    print '# Lx    = ', field['Lx']
-    print '# Ly    = ', field['Ly']
-    print '# Nx2   = ', field['Nx2']
-    print '# Ny    = ', field['Ny']
-    print '# Nz    = ', field['Nz']
-    print '# theta = ', field['thetaground']
-    print '##########################################'
+    """
+    function write_BLfield
+
+    Parameters
+    ------------
+    field: dict
+        structure produced by load_BLfield function from load_sp.py
+    filename: str
+        filename to which the current field data should be written
+    spectral: bool, optional
+        flag whether field data is in Fourier space (default is False)
+    """
+    print('##########################################')
+    print('# Writing field to file ', filename)
+    print('# Input field is spectral? ', spectral)
+    print('# ------------------------------------------')
+    print('# Field information')
+    print('# ------------------------------------------')
+    print('# time  = ', field['time'])
+    print('# Lx    = ', field['Lx'])
+    print('# Ly    = ', field['Ly'])
+    print('# Nx2   = ', field['Nx2'])
+    print('# Ny    = ', field['Ny'])
+    print('# Nz    = ', field['Nz'])
+    print('# theta = ', field['thetaground'])
+    print('##########################################')
 
     with open(filename,'wb') as binfile:
         field['time'].astype(dtype=np.float64).tofile(binfile)
